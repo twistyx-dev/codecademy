95688a65efd89f506fe267eb530b6fee93c66453
James B. Shumate <59979068+twistyx-dev@users.noreply.github.com>
2021-10-22 18:56:54 -0500

    Update DogYears.swift

--- DogYears.swift
+++ DogYears.swift
@@ -1,9 +1,14 @@
 // This is dog's age in dog years.
-var dogAge: Int = 25
+var dogAge: Double = 1.5
 // The first two years of a dog's life count as 21 human years.
-var earlyYears: Int = 21
-// Each following year counts as 4 human years.
-var laterYears: Int = (dogAge - 2) * 4
-var humanYears: Int = earlyYears
+let earlyYears: Double = 21.0
+var laterYears: Double = 0.0
+var humanYears: Double = 0.0
 
-print("My name is Mojo! Ruff ruff, I am \(humanYears) years old in human years.")
+// Each following year counts as 4 human years.
+laterYears = (dogAge - 2) * 4
+
+// Adds the two variables to determine the dogs age in human years.
+humanYears = earlyYears + laterYears
+
+print("My name is Trinity! Ruff Ruff, I am \(humanYears) years old in human years.")

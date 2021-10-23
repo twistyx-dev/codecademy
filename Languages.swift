9924427edfe0ea720ff3081e7dbe5531f3778cce
James Shumate <james@flexrentalsolutions.com>
2021-10-23 08:11:58 -0500

    Created Languages.swift

---
+++ Languages.swift
@@ -0,0 +1,17 @@
+/* Languages spoken in New York w/ abbreviations:
+eng = "English"
+sp = "Spanish"
+ch = "Chinese"
+ru = "Russian"
+*/
+var abbreviation = "sp"
+
+if abbreviation == "eng" {
+  print("English")
+} else if abbreviation == "sp" {
+  print("Spanish")
+} else if abbreviation == "ch" {
+  print("Chinese")
+} else {
+  print("Abbreviation not found")
+}

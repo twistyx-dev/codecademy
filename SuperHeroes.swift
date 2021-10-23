ff884d1bc532c84f8a13fc35bf90dd2f16a6dd4b
James Shumate <james@flexrentalsolutions.com>
2021-10-23 08:28:22 -0500

    Create SuperHeroes.swift

---
+++ SuperHeroes.swift
@@ -0,0 +1,16 @@
+var secretIdentity = "Tony Stark"
+var superheroName: String
+
+if secretIdentity == "Tony Stark" {
+  superheroName = "Iron Man"
+} else if secretIdentity == "Natasha Romanoff" {
+  superheroName = "Black Widow"
+} else if secretIdentity == "Prince T'Challa" {
+  superheroName = "Black Panther"
+} else if secretIdentity == "Thor" {
+  superheroName = "Thor"
+} else {
+  superheroName = "Unknown"
+}
+
+// Write your code below
